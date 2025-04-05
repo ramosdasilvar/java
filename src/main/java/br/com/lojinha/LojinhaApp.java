7d24c4fbe3a9de434370c288f352c70f4519a38b
Ricardo Nunes <ramosdasilvar@gmail.com>
2025-04-04 22:59:20 -0300

    Usando for

--- src/main/java/br/com/lojinha/LojinhaApp.java
+++ src/main/java/br/com/lojinha/LojinhaApp.java
@@ -38,12 +38,13 @@ public class LojinhaApp {
         System.out.println("A marca do passou a ser: " +meuProduto.getMarca());
         System.out.println("O tamanho do produto passou a ser: " +meuProduto.getTamanho());
 
+        System.out.println("Começando a apresentar os itens");
 
-        System.out.println(meuProduto.getItensInclusos().get(0).getNome());
-        System.out.println(meuProduto.getItensInclusos().get(0).getQuantidade());
-        System.out.println(meuProduto.getItensInclusos().get(1).getNome());
-        System.out.println(meuProduto.getItensInclusos().get(1).getQuantidade());
-        System.out.println(meuProduto.getItensInclusos().get(2).getNome());
-        System.out.println(meuProduto.getItensInclusos().get(2).getQuantidade());
+        for (ItemIncluso itemAtual : meuProduto.getItensInclusos()) {
+            System.out.println(itemAtual.getNome());
+            System.out.println(itemAtual.getQuantidade());
+        }
+
+        System.out.println("Acabou os itens");
     }
 }
